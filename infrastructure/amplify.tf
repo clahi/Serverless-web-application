c5d8cd5b8ebe65f4e4745ce74f7fd4c188e468ed
Abdala <clahimoha1000@gmail.com>
2024-07-18 16:15:46 +0300

    created an amplify app to host the web page

---
+++ infrastructure/amplify.tf
@@ -0,0 +1,48 @@
+resource "aws_amplify_app" "my-amplify-app" {
+  name         = "wildrydes-app"
+  repository   = "https://github.com/clahi/Serverless-web-application"
+  access_token = var.access-token
+
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - cd front-end-code
+            - yarn install
+        build:
+          commands:
+            - echo "No build step needed"
+      artifacts:
+        baseDirectory: front-end-code
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # Auto Branch Creattion
+  enable_auto_branch_creation = true
+
+  enable_branch_auto_build = true
+
+  auto_branch_creation_config {
+    # Enable auto build for the created branch.
+    enable_auto_build = true
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.my-amplify-app.id
+  branch_name = "main"
+
+  enable_auto_build = true
+
+  stage = "PRODUCTION"
+
+  environment_variables = {
+    "ENV" = "production"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,7 @@
+variable "aws-region" {
+  description = "The region of our aws infrastructure."
+  default     = "us-east-1"
+}
+
+variable "access-token" {
+}
