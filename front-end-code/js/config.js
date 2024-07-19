e35cb868a2af425ff9c15370e82df2f1cce1828b
Abdala <clahimoha1000@gmail.com>
2024-07-19 14:16:24 +0300

    Created a lambda fuciont and a rest api to expose the lambda function

--- front-end-code/js/config.js
+++ front-end-code/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SzFMMpEHY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1bll3k52s7ou1ri9hnfh0ukd5d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_lkoa1MlYC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3kqoe2j6a3e9cp7c76f0tmctrg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://n4ei2u5jr4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://55owbbvysk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
