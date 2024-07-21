96c0dfd7f87ad9593bc4639667093351f3955715
Abdala <clahimoha1000@gmail.com>
2024-07-21 11:01:15 +0300

    updated config.js with new userpoolID, userPoolClientID and invokeUrl

--- front-end-code/js/config.js
+++ front-end-code/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_fcaExCJD8', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'pkuefmamsfarc4kmlp65vodoh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_hDVqbJnWS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1vl5t2b2aursaqud4dgs0oh3k7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://9xi16t4ra4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://79otbee00m.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
